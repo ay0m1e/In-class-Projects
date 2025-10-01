b27db6f1197033bb37b2e4b78f1d43a68fd669c5
Ayomide <ay0m1ealonge@gmail.com>
2025-10-01 10:33:47 +0100

    minors

--- Python-Works/OOP.py
+++ Python-Works/OOP.py
@@ -32,3 +32,18 @@ class Teacher(Person):
 t = Teacher("Amanda","Object Oriented Programming")
 t.teach ()
         
+def fizzBuzz(n):
+    # Write your code here
+    for i in range (1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+n = 15
+
+fizzBuzz(n)
